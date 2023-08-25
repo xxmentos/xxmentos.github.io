afcc387e864db6f442591275c3b0d95d80aa6df3
xxmentos <77572698+xxmentos@users.noreply.github.com>
2023-08-25 12:00:35 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-const button = document.getElementById('btn');
-const title = document.querySelector('h1');
 
-button.addEventListener('click', () => {
-    button.style.display = 'none';
-    document.body.style.backgroundColor = 'white';
-    title.style.display = 'block';
-    title.innerText = 'Привет, я - mentos.';
-});
