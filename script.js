35d460559a9509da60954498e28d68988d3c434a
xxmentos <77572698+xxmentos@users.noreply.github.com>
2023-08-27 21:14:49 +0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const circle1 = document.getElementById("circle1");
 const circle2 = document.getElementById("circle2");
 
 function getRandomPosition() {
-  const maxX = window.innerWidth - 100; // Width of circle
-  const maxY = window.innerHeight - 100; // Height of circle
+  const maxX = window.innerWidth - 500; // Width of circle
+  const maxY = window.innerHeight - 500; // Height of circle
   const x = Math.random() * maxX;
   const y = Math.random() * maxY;
   return { x, y };
@@ -13,10 +13,6 @@ function moveCircle(circle) {
   const newPosition = getRandomPosition();
   circle.style.transform = `translate(${newPosition.x}px, ${newPosition.y}px)`;
 }
-
-// Устанавливаем случайные цвета для кругов при загрузке страницы
-circle1.style.backgroundColor = getRandomColor();
-circle2.style.backgroundColor = getRandomColor();
 
 moveCircle(circle1);
 moveCircle(circle2);
