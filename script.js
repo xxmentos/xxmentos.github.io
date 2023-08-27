ded615c397726d4dc53e014f533adb0b50f2aab2
xxmentos <77572698+xxmentos@users.noreply.github.com>
2023-08-27 21:12:29 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 const circle1 = document.getElementById("circle1");
 const circle2 = document.getElementById("circle2");
 
-function getRandomColor() {
-  const letters = "0123456789ABCDEF";
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
 function getRandomPosition() {
   const maxX = window.innerWidth - 100; // Width of circle
   const maxY = window.innerHeight - 100; // Height of circle
