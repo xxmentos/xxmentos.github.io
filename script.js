8d70f48353b29b80c074cd4808b8b64917719c6f
xxmentos <77572698+xxmentos@users.noreply.github.com>
2023-08-27 21:06:14 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
 const circle1 = document.getElementById("circle1");
 const circle2 = document.getElementById("circle2");
 
-function getRandomPosition() {
-  const maxX = window.innerWidth - 100; // Width of circle
-  const maxY = window.innerHeight - 100; // Height of circle
-  const x = Math.random() * maxX;
-  const y = Math.random() * maxY;
-  return { x, y };
-}
-
 function getRandomColor() {
   const letters = "0123456789ABCDEF";
   let color = "#";
@@ -18,12 +10,25 @@ function getRandomColor() {
   return color;
 }
 
+function getRandomPosition() {
+  const maxX = window.innerWidth - 100; // Width of circle
+  const maxY = window.innerHeight - 100; // Height of circle
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+  return { x, y };
+}
+
 function moveCircle(circle) {
   const newPosition = getRandomPosition();
-  const newColor = getRandomColor();
   circle.style.transform = `translate(${newPosition.x}px, ${newPosition.y}px)`;
-  circle.style.backgroundColor = newColor;
 }
+
+// Устанавливаем случайные цвета для кругов при загрузке страницы
+circle1.style.backgroundColor = getRandomColor();
+circle2.style.backgroundColor = getRandomColor();
+
+moveCircle(circle1);
+moveCircle(circle2);
 
 setInterval(() => {
   moveCircle(circle1);
